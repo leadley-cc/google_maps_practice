8b23b660b6ec49187447e363b4d9c09e5057c560
Michael Leadley <leadley75@gmail.com>
2017-10-17 19:35:50 +0100

    Add InfoWindow

--- public/app.js
+++ public/app.js
@@ -3,9 +3,17 @@ var initialise = function () {
   // var coords = { lat: 40.712784, lng: -74.005941 }
   var coords = { lat: 63.6195751, lng: -19.5991159 }
   var mainMap = new MapWrapper(mapDiv, coords, 10)
-  mainMap.addMarker(coords)
+  var eyjaMarker = mainMap.addMarker(coords)
+  var infowindow = new google.maps.InfoWindow({
+    content: "Say after me: [ˈeɪjaˌfjatlaˌjœːkʏtl̥]"
+  })
+  eyjaMarker.addListener("click", function() {
+    infowindow.open(mainMap, eyjaMarker)
+  })
+
   mainMap.addMarker({ lat: 36.476945, lng: 53.559506 })
   mainMap.addMarker({ lat: 39.334936, lng: -78.781114 })
+
   mainMap.addClickEvent()
 
   var bounceButton = document.getElementById("bounce-markers")
